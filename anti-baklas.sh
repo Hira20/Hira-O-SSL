6740809f8df94811844413683a07ba79a1bdc702
jvb20 <48585021+jvb20@users.noreply.github.com>
2019-03-31 12:34:49 +0000

    Update anti-baklas.sh

--- anti-baklas.sh
+++ anti-baklas.sh
@@ -35,49 +35,6 @@ $MODPROBE ip_conntrack_irc
 # Hardcode static ARP cache entries here
 # $ARP -s IP-ADDRESS MAC-ADDRESS
  
-# Kernel configuration.
-#------------------------------------------------------------------------------
- 
-# Disable IP forwarding.
-# On => Off = (reset)
-echo 1 > /proc/sys/net/ipv4/ip_forward
-echo 0 > /proc/sys/net/ipv4/ip_forward
- 
-# Enable IP spoofing protection
-for i in /proc/sys/net/ipv4/conf/*/rp_filter; do echo 1 > $i; done
- 
-# Protect against SYN flood attacks
-echo 1 > /proc/sys/net/ipv4/tcp_syncookies
- 
-# Ignore all incoming ICMP echo requests
-echo 0 > /proc/sys/net/ipv4/icmp_echo_ignore_all
- 
-# Ignore ICMP echo requests to broadcast
-echo 1 > /proc/sys/net/ipv4/icmp_echo_ignore_broadcasts
- 
-# Log packets with impossible addresses.
-for i in /proc/sys/net/ipv4/conf/*/log_martians; do echo 1 > $i; done
- 
-# Don't log invalid responses to broadcast
-echo 1 > /proc/sys/net/ipv4/icmp_ignore_bogus_error_responses
- 
-# Don't accept or send ICMP redirects.
-for i in /proc/sys/net/ipv4/conf/*/accept_redirects; do echo 0 > $i; done
-for i in /proc/sys/net/ipv4/conf/*/send_redirects; do echo 0 > $i; done
- 
-# Don't accept source routed packets.
-for i in /proc/sys/net/ipv4/conf/*/accept_source_route; do echo 0 > $i; done
- 
-# Disable proxy_arp.
-for i in /proc/sys/net/ipv4/conf/*/proxy_arp; do echo 0 > $i; done
- 
-# Enable secure redirects, i.e. only accept ICMP redirects for gateways
-# Helps against MITM attacks.
-for i in /proc/sys/net/ipv4/conf/*/secure_redirects; do echo 1 > $i; done
- 
-# Disable bootp_relay
-for i in /proc/sys/net/ipv4/conf/*/bootp_relay; do echo 0 > $i; done
- 
 # Default policies.
 #------------------------------------------------------------------------------
  
